fc0eb03b7029d6c6b7fd2c1bcb8de40d62d21acd
hiroki0104 <hiroki010419@outlook.jp>
2020-01-09 01:32:21 +0900

    component_crate

--- hello_world/src/App.js
+++ hello_world/src/App.js
@@ -1,16 +1,30 @@
 import React from 'react';
 
 
-function App() {
+// function App() {
+//   return (
+//     <React.Fragment>
+//       <label htmlFor="bar">
+//         bar
+//       </label>
+//       <input type="text" />
+//     </React.Fragment>
+//     // <button type="text" onClick={() => alert("I'm clicked")} />
+//   );
+// }
+
+const App = () => {
   return (
-    <React.Fragment>
-      <label htmlFor="bar">
-        bar
-      </label>
-      <input type="text" />
-    </React.Fragment>
-    // <button type="text" onClick={() => alert("I'm clicked")} />
-  );
+    <div>
+      <Cat />
+      <Cat />
+      <Cat />
+    </div>
+  )
+}
+
+const Cat = () => {
+  return <div>Meow!</div>
 }
 
 export default App;
